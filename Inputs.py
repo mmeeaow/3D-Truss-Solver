9f5ce1e25398674551f7c95e2f64bd92f2dee2bd
mmeeaow <aliaadil142@gmail.com>
2024-11-17 01:26:59 +0500

    v1.15 completed inputs file

--- Inputs.py
+++ Inputs.py
@@ -2,9 +2,9 @@
 
 import tkinter as k
 from tkinter import ttk
-from tkinter import filedialog
-from os import cpu_count
-
+from tkinter import filedialog, messagebox
+from pandas import read_csv
+import psutil
 
 def update_units(*args):
     # This function is called whenever the user selects a unit from the dropdown menu.
@@ -30,7 +30,27 @@ def submit():
     dimension = dimension_combo.get()
     Youngs_mod_unit = Youngs_modulus_combo.get()
     force_unit = force_combo.get()
-    core_usage = num_core_var.get()
+    core_usage = num_cores_var.get()
+
+def select_input_file():
+    user_file = filedialog.askopenfilename(
+        title="Select Input File",
+        filetypes=[("CSV Files", "*.csv")]
+    )
+
+    if user_file:
+        print(f"File Selected: {user_file}")
+        try:
+            inputs = read_csv(user_file, delimiter = ",", skiprows = 1, dtype = float)
+            if inputs.shape[1] != 10:
+               messagebox.showerror("Error", "The selected file was not readable. Please ensure it has the correct number of columns.")
+               return None
+            else:
+                return inputs
+        except Exception as e:
+            messagebox.showerror("Error", "Error: The file selected is not a valid CSV file.\n" + str(e))
+            return None
+    return None
 
 
 #main window
@@ -47,7 +67,7 @@ unit_system_combo['values'] = ("Metric", "Imperial")
 unit_system_combo.pack()
 
 #update the units based on the selected unit system
-unit_system_var.trace_add("w", update_units)
+unit_system_var.trace_add("write", update_units)
 
 #drop down menu for the dimension units
 dimension_label = k.Label(root, text="Select Dimension Unit:")
@@ -67,19 +87,20 @@ force_label.pack()
 force_combo = ttk.Combobox(root)
 force_combo.pack()
 
-available_cpu_cores = cpu_count()
-num_core_var = k.StringVar(value="1")
-# Drop-down for number of CPU cores
-num_cores_label = k.Label(root, text="Select Number of CPU Cores:")
-num_cores_label.pack()
-num_cores_combo = ttk.Combobox(root, textvariable=num_core_var)
-num_cores_combo['values'] = [str(i) for i in range(1, available_cpu_cores - 1)]  # Options from 1 to available cores
+available_cpu_cores = psutil.cpu_count(logical = False)
+num_cores_var = k.StringVar(value="1")
+core_label = k.Label(root, text="Select Number of CPU Cores:")
+core_label.pack()
+num_cores_combo = ttk.Combobox(root, textvariable = num_cores_var)
+num_cores_combo['values'] = [str(i) for i in range(1, available_cpu_cores + 1)]
 num_cores_combo.pack()
-
 update_units()
 
 submit_button = k.Button(root, text="submit", command=submit)
-submit_button.pack()
+submit_button.pack(padx = 15, pady = 21)
+
+select_button2 = k.Button(root, text="Select CSV File", command=select_input_file)
+select_button2.pack(padx=10, pady=21)
 
 
 
